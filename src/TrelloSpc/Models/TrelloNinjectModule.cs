6bfe528d85cde3335d45035de5c5e937419a83a7
stroiman <peter@stroiman.com>
2012-10-08 13:51:03 +0200

    Implemented Ninject activation of controllers.

---
+++ src/TrelloSpc/Models/TrelloNinjectModule.cs
@@ -0,0 +1,17 @@
+﻿using Ninject.Modules;
+using Ninject.Extensions.Conventions;
+
+namespace TrelloSpc.Models
+{
+    public class TrelloNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind(x => x
+                               .FromThisAssembly()
+                               .SelectAllClasses()
+                               .BindDefaultInterface()
+                );
+        }
+    }
+}
