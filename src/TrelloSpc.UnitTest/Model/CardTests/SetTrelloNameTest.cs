1c6034e5f49af0700711afb23aaf88fde785ed88
stroiman <peter@stroiman.com>
2012-10-10 10:56:23 +0200

    Moved unit test for Card.Name and Card.Points

--- src/TrelloSpc.UnitTest/Model/CardTests/SetTrelloNameTest.cs
+++ src/TrelloSpc.UnitTest/Model/CardTests/SetTrelloNameTest.cs
@@ -1,41 +1,41 @@
 ﻿using NUnit.Framework;
 using TrelloSpc.Models;
 
-namespace TrelloSpc.UnitTest.Model
+namespace TrelloSpc.UnitTest.Model.CardTests
 {
     [TestFixture]
-    public class CardTest
+    public class SetTrelloNameTest
     {
         [Test]
-        public void NameShouldReturnTrelloNameWhenNoStoryPointGiven()
+        public void ShouldInitializeNameWhenNoPointsGiven()
         {
             var card = new Card { TrelloName = "Name" };
             Assert.That(card.Name, Is.EqualTo("Name"));
         }
 
         [Test]
-        public void NameShouldReturnStrippedTrelloNameWhenPointGiven()
+        public void ShouldStripPointsFromNameWhenPointsGiven()
         {
             var card = new Card { TrelloName = "(5) Name" };
             Assert.That(card.Name, Is.EqualTo("Name"));
         }
 
         [Test]
-        public void PointShouldBeNullWhenNoPointsGiven()
+        public void ShouldSetPointsToNullWhenNoPointsGiven()
         {
             var card = new Card { TrelloName = "Name" };
             Assert.That(card.Points, Is.Null);
         }
 
         [Test]
-        public void PointShouldEqualCardPointsWhenGiven()
+        public void ShouldSetPointsWhenPointsGiven()
         {
             var card = new Card { TrelloName = "(5) Name" };
             Assert.That(card.Points, Is.EqualTo(5));
         }
 
         [Test]
-        public void ResettingTrelloNameShouldResetPointAndName()
+        public void ShouldResetNameAndPointsWhenAssignedToNull()
         {
             var card = new Card { TrelloName = "(5) Name" };
             card.TrelloName = null;
