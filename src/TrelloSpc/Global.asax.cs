dd379420488682e02d466dc2e9f608b68101eb20
stroiman <peter@stroiman.com>
2012-10-10 12:25:28 +0200

    Attempt to fix problem

--- src/TrelloSpc/Global.asax.cs
+++ src/TrelloSpc/Global.asax.cs
@@ -18,7 +18,7 @@ namespace TrelloSpc
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            //WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
